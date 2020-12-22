cf240fcd23b8607ec2e40e7e0d1d687a6b967c11
Go Sato <gousato07@gmail.com>
2020-12-22 15:08:09 +0900

    出力fpsを自由に決定可能

--- StereoAVIToPNG.py
+++ StereoAVIToPNG.py
@@ -6,20 +6,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--path_avi", help="video_path", type=str)
 parser.add_argument("--path_output_png", help="output png path", type=str)
 parser.add_argument("--option", help="top or middle or bottom", type=str)
+parser.add_argument("--fps", help="output_fps", type=str)
 
 args = parser.parse_args()
 
 path_avi = args.path_avi
 path_output_png = args.path_output_png
 option = args.option
+fps = float(args.fps)
 
-def extract_frames(path_avi, path_output_png, option="None"):
+def extract_frames(path_avi, path_output_png, option, fps):
     num = 0
     if not os.path.exists(path_output_png):
         os.makedirs(path_output_png)
 
     cap = cv2.VideoCapture(path_avi)
     count = 0
+    avi_fps = 30
+    freq = int(avi_fps / fps)
 
     while cap.isOpened():
 
@@ -27,7 +31,7 @@ def extract_frames(path_avi, path_output_png, option="None"):
 
         if ret:
             num += 1
-            if num % 3 == 0:
+            if num % freq == 0:
                 init_height, init_width = frame.shape[:2]
                 height = init_height // 3
                 width = init_width
@@ -39,7 +43,7 @@ def extract_frames(path_avi, path_output_png, option="None"):
                     frame = frame[(height * 2):(height * 3), 0:width]
                 cv2.imwrite(os.path.join(path_output_png, "frame_{:06d}.png".format(count)), frame)
                 count += 1
-            if num % 300 == 0:
+            if count % 100 == 0:
                 print(count)
         else:
             break
